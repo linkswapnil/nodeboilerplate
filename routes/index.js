6128933970a67e40dd1b54a946dc3d18f1710d58
Swapnil Navalakha <swapnil.navalakha@taranawireless.com>
2019-09-01 21:34:38 +0530

    my node module

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 /* eslint-disable new-cap */
+import {circleUtil, squareUtil} from '../utils';
+
 const express = require('express');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-	res.render('index');
+	const context = {
+		circleArea: circleUtil.area(100),
+		squareArea: squareUtil.area(200)
+	};
+	res.render('index', context);
 });
 
 module.exports = router;
