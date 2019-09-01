a65c54fbb50bbca0eb8888178d6845f92d5ac737
Swapnil Navalakha <swapnil.navalakha@taranawireless.com>
2019-09-01 22:11:52 +0530

    fix test

--- routes/index.spec.js
+++ routes/index.spec.js
@@ -11,10 +11,14 @@ describe('Index Router', () => {
 		chai.request(server)
 			.get('/')
 			.end((err, res) => {
-				res.should.have.status(200);
-				res.body.should.be.a('array');
-				res.body.length.should.be.eql(0);
-				done();
+				try {
+					res.should.have.status(200);
+					res.body.should.be.a('array');
+					res.body.length.should.be.eql(0);
+					done();
+				}catch (e) {
+					done(e);
+				}
 			});
 	});
 });
